c9faf55325c11548f595a2907dd1e9f3b8298277
Tymon Tobolski <i@teamon.eu>
2018-11-18 16:10:35 +0100

    Dummy redis store

--- mix.exs
+++ mix.exs
@@ -69,6 +69,7 @@ defmodule Tesla.Mixfile do
       # other
       {:fuse, "~> 2.4", optional: true},
       {:calendar, "~> 0.17", optional: true},
+      {:redix, "~> 0.7", optional: true},
 
       # testing & docs
       {:excoveralls, "~> 0.8", only: :test},
