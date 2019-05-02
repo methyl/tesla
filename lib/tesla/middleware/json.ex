468e7bceee8a02d970f3c6403b6a855ee76793d9
Lucjan Suski <lucjansuski@gmail.com>
2019-05-02 17:56:43 +0200

    Correct dependency definition for Jason

--- lib/tesla/middleware/json.ex
+++ lib/tesla/middleware/json.ex
@@ -6,7 +6,7 @@ defmodule Tesla.Middleware.JSON do
 
   This middleware requires [jason](https://hex.pm/packages/jason) (or other engine) as dependency.
 
-  Remember to add `{:jason, ">= 1.0"}` to dependencies
+  Remember to add `{:jason, "~> 1.1"}` to dependencies
   Also, you need to recompile tesla after adding `:jason` dependency:
 
   ```
